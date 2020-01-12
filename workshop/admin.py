5e57513920eeafde9c03bc86c99e3283af2558ae
Maciej <maciek2tr99@gmail.com>
2020-01-12 17:20:06 +0100

    Add workshop model

--- workshop/admin.py
+++ workshop/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Workshop, WorkshopRegistration
+from django.http import HttpResponse
+import csv
 
-# Register your models here.
+
+class WorkshposRegistrationsAdmin(admin.ModelAdmin):
+    actions = ["export_as_csv"]
+
+    def export_as_csv(self, request, queryset):
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Export Selected"
+
+
+admin.site.register(WorkshopRegistration, WorkshposRegistrationsAdmin)
+admin.site.register(Workshop)
--- workshop/models.py
+++ workshop/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils.timezone import now
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Workshop(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    date = models.DateField()
+    open = models.BooleanField(default=True)
+    auto_response = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name + '(' + str(self.id) + ')'
+
+
+class WorkshopRegistration(models.Model):
+    workshops_id = models.ForeignKey(Workshop, on_delete=models.CASCADE)
+    participant_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField(default=now)
+    active = models.BooleanField()
+    accepted = models.BooleanField()
