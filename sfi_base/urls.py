c22e25132949e7aa17289e13486e5cbd9c008eee
Adam Pardyl <adam@pardyl.com>
2020-02-03 13:25:26 +0100

    Logout with POST

--- sfi_base/urls.py
+++ sfi_base/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import url
 from django.urls import include
 
-from .views import OIDCLogoutView, OIDCAuthenticationNoPromptRequestView
+from .views import OIDCAuthenticationNoPromptRequestView
 
 urlpatterns = [
-    url(r'^logout/$', OIDCLogoutView.as_view(), name='oidc_logout'),
     url(r'^tryauthenticate/', OIDCAuthenticationNoPromptRequestView.as_view(), name='oidc_try_authenticate'),
     url(r'^', include('mozilla_django_oidc.urls')),
 ]
--- sfi_base/views.py
+++ sfi_base/views.py
@@ -1,14 +1,4 @@
-from mozilla_django_oidc import views
 from mozilla_django_oidc.views import OIDCAuthenticationRequestView
-
-
-class OIDCLogoutView(views.OIDCLogoutView):
-    def get(self, request):
-        """
-        The OIDCLogoutView only implements logout via a post request, but we want a get request.
-        Therefore allow that here
-        """
-        return self.post(request)
 
 
 class OIDCAuthenticationNoPromptRequestView(OIDCAuthenticationRequestView):
